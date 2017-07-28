a2a0819ccc1a713b116f847b3f20f3628bd778ee
Aleksandra Malesa <a_malesa@hotmail.co.uk>
2017-07-28 19:37:43 +0100

    fix test

--- src/client/app/users/users.controller.spec.js
+++ src/client/app/users/users.controller.spec.js
@@ -1,6 +1,7 @@
 /* jshint -W117, -W030 */
 describe('UsersController', function () {
   var controller;
+  var people = mockData.getMockPeople();
 
   beforeEach(function () {
     bard.appModule('app.users');
@@ -8,6 +9,7 @@ describe('UsersController', function () {
   });
 
   beforeEach(function () {
+    sinon.stub(dataservice, 'getPeople').returns($q.when(people));
     controller = $controller('UsersController');
     $rootScope.$apply();
   });
@@ -19,23 +21,20 @@ describe('UsersController', function () {
     it('should be created successfully', function () {
       expect(controller).to.be.defined;
     });
-    /*
+
     describe('after activate', function () {
-      it('should have title of Admin', function () {
+
+      it('should have title of Dashboard', function () {
         expect(controller.title).to.equal('Users');
       });
 
       it('should have logged "Activated"', function () {
         expect($log.info.logs).to.match(/Activated/);
       });
-    });
 
-    it('should have at least 1 user', function () {
-      expect(controller.userList).to.have.length.above(0);
+      it('should have at least 1 person', function () {
+        expect(controller.userList).to.have.length.above(0);
+      });
     });
-
-    it('should have at least 1 country', function () {
-      expect(controller.locations).to.have.length.above(0);
-    });*/
   });
 });
